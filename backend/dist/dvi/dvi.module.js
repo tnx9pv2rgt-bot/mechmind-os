716f0da94bbc9904a2604840983aee95f2441ff4
MechMind Deploy <deploy@mechmind.local>
2026-03-08 14:10:54 +0100

    fix: add NotificationsModule import to DviModule

--- backend/dist/dvi/dvi.module.js
+++ backend/dist/dvi/dvi.module.js
@@ -12,18 +12,18 @@ const inspection_controller_1 = require("./controllers/inspection.controller");
 const inspection_service_1 = require("./services/inspection.service");
 const prisma_service_1 = require("../common/services/prisma.service");
 const s3_service_1 = require("../common/services/s3.service");
-const notifications_service_1 = require("../notifications/services/notifications.service");
+const notifications_module_1 = require("../notifications/notifications.module");
 let DviModule = class DviModule {
 };
 exports.DviModule = DviModule;
 exports.DviModule = DviModule = __decorate([
     (0, common_1.Module)({
+        imports: [notifications_module_1.NotificationsModule],
         controllers: [inspection_controller_1.InspectionController],
         providers: [
             inspection_service_1.InspectionService,
             prisma_service_1.PrismaService,
             s3_service_1.S3Service,
-            notifications_service_1.NotificationsService,
         ],
         exports: [inspection_service_1.InspectionService],
     })
--- backend/src/dvi/dvi.module.ts
+++ backend/src/dvi/dvi.module.ts
@@ -7,15 +7,15 @@ import { InspectionController } from './controllers/inspection.controller';
 import { InspectionService } from './services/inspection.service';
 import { PrismaService } from '../common/services/prisma.service';
 import { S3Service } from '../common/services/s3.service';
-import { NotificationsService } from '../notifications/services/notifications.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [InspectionController],
   providers: [
     InspectionService,
     PrismaService,
     S3Service,
-    NotificationsService,
   ],
   exports: [InspectionService],
 })
